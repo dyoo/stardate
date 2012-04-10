637f90b2d15d4eeb07a7c1433781079de438391b
Danny Yoo <dyoo@cs.wpi.edu>
2012-04-10 16:12:09 -0400

    minimal info.rkt

---
+++ info.rkt
@@ -0,0 +1,13 @@
+#lang setup/infotab
+(define name "stardate: compute the current TNG stardate")
+(define categories '(misc))
+(define can-be-loaded-with 'all)
+(define required-core-version "5.1.1")
+(define version "1.0")
+(define repositories '("4.x"))
+(define scribblings '(("manual.scrbl")))
+(define primary-file "main.rkt")
+(define blurb 
+  '("stardate: compute the current TNG stardate."))
+(define release-notes
+  '((p "First release.")))
