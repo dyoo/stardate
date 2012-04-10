0a9ed51b80e32f77a4b94c1f4ecd476e758a752e
Danny Yoo <dyoo@cs.wpi.edu>
2012-04-10 15:58:02 -0400

    cleaning up code

--- tng-stardate.rkt
+++ tng-stardate.rkt
@@ -2,24 +2,18 @@
 
 (require racket/date)
 
+(provide date->stardate current-stardate)
+
 ;; http://trekguide.com/Stardates.htm
+;; http://en.memory-alpha.org/wiki/Stardate
 
 ;; There are three ways to produce stardates, of which I'm choosing
 ;; TNG's approach.
 
+;; According to trekguide.com, we get this constant:
+(define days-per-year 365.2422)
 
-;; http://en.memory-alpha.org/wiki/Stardate
-
-
-;; According to:
-;;
-;;     http://megiddo.wikidot.com/stardate
-;;
-;; we can compute the number of stardate units per day with this calculation.
-
-;; http://www.enotes.com/science/q-and-a/what-exact-number-days-one-year-179507
-(define days-per-year (/ 146097 400))
-
+;; According to trekguide.com, we get this constant:
 (define stardate-units-per-year 1000)
 
 (define stardate-units-per-day (/ stardate-units-per-year
@@ -31,18 +25,40 @@
         24 ;; 24 hours per day
         )))
 
-
 ;; Season 1 was in the 41000's
 ;; Season 2 was in the 42000's
 ;; ...
 
-
-;; Season 1 Episode 1, Encounter at Farpoint was on April 13, 1987:
+;; Season 1 Episode 1, Encounter at Farpoint was on September 28, 1987:
 ;; http://en.memory-alpha.org/wiki/Encounter_at_Farpoint_(episode)
 ;; Stardate 41153.7
 
-(define seconds-at-jan-1-1987 (find-seconds 0 0 0 1 1 1987 #f))
-(define seconds-at-april-13-1987 (find-seconds 0 0 0 13 4 1987 #f))
 
-(+ 41000 (* stardate-units-per-second (- seconds-at-april-13-1987
-                                         seconds-at-jan-1-1987)))
+;; According to:
+;;
+;; http://trekguide.com/stardateToday.js
+;;
+;; the stardate 41000 starts at July 15, 1987 00:00:00
+(define the-epoch-seconds (find-seconds 00 00 00
+                                        15
+                                        7  ;; July
+                                        1987))
+
+;; date-stardate-string: date -> string
+(define (date->stardate a-date)
+  (stardate-string (date->stardate-number a-date)))
+
+
+;; date->stardate: date -> number
+(define (date->stardate-number a-date)
+  (define secs (date->seconds a-date))
+  (define the-stardate (+ 41000 (* stardate-units-per-second (- secs the-epoch-seconds))))
+  the-stardate)
+
+;; stardate-string: number -> string
+(define (stardate-string a-stardate)
+  (number->string (exact->inexact (/ (floor (* a-stardate 10)) 10))))
+
+(define (current-stardate)
+  (date->stardate (current-date)))
+
